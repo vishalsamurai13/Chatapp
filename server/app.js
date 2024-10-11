d06af6c7dda82411486272b5b5ee7c5b0af302af
Vishal Sharma <133427352+vishalsamurai13@users.noreply.github.com>
2024-10-11 12:29:48 +0530

    1st commit

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+const authRouter = require('./controllers/authController');
+
+//use auth controller router
+app.use(express.json());
+app.use('/api/auth', authRouter);
+
 module.exports = app;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,39 @@
 const router = require('express').Router();
+const User = require('./../models/user');
+const bcrypt = require('bcryptjs');
+
+router.post('/signup', async (req,res) => {
+    try {
+        //1. If the user already exists
+        const user = await User.findOne({ email: req.body.email});
+
+        //2. If user exists, send an error response
+        if(user){
+            return res.send({
+                message: "User Already exists!",
+                success: false
+            });
+        }
+
+        //3. encrypt the password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        req.body.password = hashedPassword;
+
+        //4. Create new user, save in db
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        res.send({
+            message: "User Created Successfully!",
+            success: true
+        });
+        
+    } catch (error) {
+        res.send({
+            message: error.message,
+            success: false
+        });
+    }
+})
+
+module.exports = router;
