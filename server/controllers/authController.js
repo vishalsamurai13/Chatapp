463ac8babd50813c6ba8df8bb174a5a05f54a5ec
Vishal Sharma <133427352+vishalsamurai13@users.noreply.github.com>
2024-10-13 03:12:03 +0530

    .

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('./../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req,res) => {
     try {
@@ -27,12 +28,49 @@ router.post('/signup', async (req,res) => {
             message: "User Created Successfully!",
             success: true
         });
-        
+
     } catch (error) {
         res.send({
             message: error.message,
             success: false
         });
+    }
+})
+
+router.post('/login', async (req,res) => {
+    try {
+        //1. check if user exists
+        const user = await User.findOne({ email: req.body.email });
+        if(!user){
+            res.send({
+                message: "User doesn't exist",
+                success: false
+            })
+        }
+
+        //2. check is the password is correct
+        const isValid = await bcrypt.compare(req.body.password, user.password);
+        if(!isValid){
+            res.send({
+                message: "User doesn't exist",
+                success: false
+            })
+        }
+
+        //3. if the user exists and password is correct, assign a jwt
+        const token = jwt.sign({userId: user._id}, process.env.SECRET_KEY, {expiresIn: "1d"});
+
+        res.send({
+            message: "User logged in successfully",
+            success: true,
+            token: token
+        });
+
+    } catch (error) {
+        res.send({
+            message: error.message,
+            success: false
+        })
     }
 })
 
---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req,res,next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY); //{userId: user._id}
+
+        req.body.userId = decodedToken.userId;
+
+        next();
+    } catch (error) {
+        res.send({
+            message: error.message,
+            success: false
+        })
+    }
+}
