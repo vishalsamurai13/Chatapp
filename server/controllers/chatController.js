c777d058f5a0a83792c7d20bb789eee0e3918a57
Vishal Sharma <133427352+vishalsamurai13@users.noreply.github.com>
2024-10-15 21:29:07 +0530

    added new files

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,23 +1,40 @@
-const router = require('express').Router();
-const Chat = require('./../models/chat');
-const authMiddleware = require('./../middlewares/authMiddleware');
+const router = require("express").Router();
+const Chat = require("./../models/chat");
+const authMiddleware = require("./../middlewares/authMiddleware");
 
-router.post('/create-new-chat', authMiddleware, async (req,res) => {
-    try {
-        const chat = new Chat(req.body);
-        const savedChat = await chat.save();
+router.post("/create-new-chat", authMiddleware, async (req, res) => {
+  try {
+    const chat = new Chat(req.body);
+    const savedChat = await chat.save();
 
-        res.status(201).send({
-            message: "Chat created successfully",
-            success: true,
-            data: savedChat
-        })
-    } catch (error) {
-        res.status(400).send({
-            message: error.message,
-            success: false
-        })
-    }
-})
+    res.status(201).send({
+      message: "Chat created successfully",
+      success: true,
+      data: savedChat,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+router.get("/get-all-chat", authMiddleware, async (req, res) => {
+  try {
+    const allChats = await Chat.find({ members: { $in: req.body.userId } });
+
+    res.status(200).send({
+      message: "Chat fetched successfully",
+      success: true,
+      data: allChats,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
 
 module.exports = router;
