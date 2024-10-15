49bf08c5d8e8a605a156a34bf32064ca3fbc7901
Vishal Sharma <133427352+vishalsamurai13@users.noreply.github.com>
2024-10-15 21:52:32 +0530

    modified

--- server/models/message.js
+++ server/models/message.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-  chatId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "chats",
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "chats",
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
   },
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  read: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("messages", messageSchema);
