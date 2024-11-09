19773da294ed76fe97cddd31e587c9f74c4d1fb7
Vishal Sharma <133427352+vishalsamurai13@users.noreply.github.com>
2024-11-09 15:16:50 +0530

    added clearUnreadMessageCount api

--- client/src/apiCalls/chat.js
+++ client/src/apiCalls/chat.js
@@ -17,3 +17,12 @@ export const createNewChat = async (members) => {
         return error;
     }
 }
+
+export const clearUnreadMessageCount = async (chatId) => {
+    try {
+        const response = await axiosInstance.post('api/chat/clear-unread-message', { chatId: chatId });
+        return response.data;
+    } catch (error) {
+        return error;
+    }
+}
